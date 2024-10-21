29abd25575e3c1824da9f6559b09b9502f7d1c3c
Hakim <mohamed.hakim.dev@gmail.com>
2024-10-21 19:18:45 +0100

    TP_1

---
+++ TP_1/Exercice_1/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Saisir le nombre des étudiants :");
+        int nbEtudiants = sc.nextInt();
+        int[] notes = new int[nbEtudiants];
+
+        for (int i = 0; i < nbEtudiants; i++) {
+            System.out.println("Saisir la note d'étudiant num: "+(i+1));
+            notes[i] = sc.nextInt();
+        }
+
+        // Initial Grades
+        //System.out.println(Arrays.toString(notes));
+
+        // Sort
+        NoteManager.sortGrades(notes);
+        System.out.println(Arrays.toString(notes));
+
+        // Average
+        int average = NoteManager.getAverageGrade(notes);
+        System.out.println("La note moyenne : "+average);
+
+
+        // Low _ High
+        System.out.println(NoteManager.getHighLowGrade(notes));
+
+        // Occurences
+        System.out.println("Saisir la note que vouz voulez rechercher : ");
+        int note = sc.nextInt();
+        int nbOccurrences = NoteManager.getGradeOccurence(notes, note);
+        System.out.println(nbOccurrences+" étudiants ont la note "+note);
+    }
+}
---
+++ TP_1/Exercice_4/src/CharCalculator.java
@@ -0,0 +1,36 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CharCalculator {
+
+    // let's create a sorted table of alphabets
+
+
+    public static void runCalculator(String input) {
+        Character[] alphabets = new Character[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        List<Character> alphabetList = Arrays.asList(alphabets);
+        int[] nb_occurences = new int[26];
+
+        // make all characters in input lowercase
+        String str = input.toLowerCase();
+        // Remove Spaces
+        str = str.replaceAll("\\s", "");
+
+        // Count Occurences and Fill Table
+        for(Character c : str.toCharArray()) {
+            int index = alphabetList.indexOf(c);
+            nb_occurences[index]++;
+        }
+
+        // Print Result
+        System.out.println("La chaine "+input+" contient :");
+        for(int i = 0; i < 26; i++) {
+            if(nb_occurences[i] > 0) {
+                System.out.println(nb_occurences[i] +" fois la lettre '"+alphabets[i]+"'");
+            }
+        }
+    }
+}
+
